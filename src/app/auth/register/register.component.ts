cbe481434e2d87cf2c7e861f5687821511c2b69a
Diego Pagini <dq@qbkconsulting.com>
2021-01-19 08:16:34 -0300

    registro con firebase listo

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: "app-register",
@@ -9,7 +11,11 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent implements OnInit {
   registroFrom: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.registroFrom = this.fb.group({
@@ -20,6 +26,16 @@ export class RegisterComponent implements OnInit {
   }
 
   crearUsuario() {
-    console.log(this.registroFrom.valid, this.registroFrom.value);
+    if (this.registroFrom.invalid) {
+      return;
+    }
+    const { nombre, correo, password } = this.registroFrom.value;
+    this.authService
+      .crearUsuario(nombre, correo, password)
+      .then((credenciales) => {
+        console.log(credenciales);
+        this.router.navigate(["/"]);
+      })
+      .catch((err) => console.error(err));
   }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  constructor(private auth: AngularFireAuth) {}
+
+  crearUsuario(nombre: string, email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+}
