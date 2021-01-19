916eef2a104d5b27fa975d7119f301496594da66
Diego Pagini <dq@qbkconsulting.com>
2021-01-19 15:02:11 -0300

    implementando redux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "./services/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private authService: AuthService) {
+    this.authService.initAuthListener();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { map } from "rxjs/operators";
+import { Usuario } from "../models/usuario.models";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {}
+
+  initAuthListener() {
+    this.auth.authState.subscribe((firebaseUser) => {
+      console.log(firebaseUser);
+    });
+  }
 
   crearUsuario(nombre: string, email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new Usuario(user.uid, nombre, user.email);
+        return this.firestore.doc(`${user.uid}/usuario`).set({ ...newUser });
+      });
   }
 
   loginUsuario(email: string, password: string) {
@@ -17,5 +34,11 @@ export class AuthService {
 
   logout() {
     return this.auth.signOut();
+  }
+
+  isAuth() {
+    return this.auth.authState.pipe(
+      map((firebaseUser) => firebaseUser != null)
+    );
   }
 }
